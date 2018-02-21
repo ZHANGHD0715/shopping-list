d01bf4dd8311d77a8291156b4592922b146d4723
HaiDong Zhang <haidonghi@126.com>
2018-02-21 17:16:36 +0800

    [HaiDong] Initial code

---
+++ app/src/main/java/com/example/hdzhang/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.hdzhang.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity  {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage(view);
+            }
+        });
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        startActivity(intent);
+    }
+
+//    @Override
+//    public void onClick(View view) {
+//        sendMessage(btn);
+//    }
+}
